0be6a78ae57a1cf518b9e6bc99febf599e25e94b
Nielark <markdanielmagpantay2001@gmail.com>
2023-03-20 20:45:58 +0800

    modify the looping

--- Binary.go
+++ Binary.go
@@ -13,20 +13,32 @@ func binaryToDecimal() {
 	var decimal int
 	var binaryHolder int
 	var binaryArr [100]int
-	ctr := 0
+	ctr := -1
 	array := [9]int{256, 128, 64, 32, 16, 8, 4, 2, 1}
 
 	// Input a binary and store it in the array
 	fmt.Print("Enter a binary digit: ")
 	for binaryHolder != 2 {
+		ctr++
 		fmt.Scan(&binaryHolder)
 		binaryArr[ctr] = binaryHolder
-		ctr++
 	}
-	ctr--
+
+	max := 9 // Max size if the array
+	if ctr != 9 {
+		for i := ctr; i >= 0; i-- {
+			binaryArr[max] = binaryArr[i]
+			binaryArr[i] = 0
+			max--
+		}
+	}
+
+	for i := 0; i < 9; i++ {
+		fmt.Print(binaryArr[i])
+	}
 
 	// Convert the binary into decimal
-	for i := 0; i < ctr; i++ {
+	for i := 0; i < 9; i++ {
 		if binaryArr[i] == 1 { // Check if the bit is equal to one
 			decimal += array[i] // Add the values in the array and stored it in the decimal if the the condition is true
 		}
