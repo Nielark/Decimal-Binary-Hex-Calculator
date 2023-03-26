9b528fd68f9f91c4dedcf7f26c3a9442a65db071
Nielark <markdanielmagpantay2001@gmail.com>
2023-03-26 18:47:57 +0800

    problems with the hexToBinary was fixed and now shows correct output

--- Binary.go
+++ Binary.go
@@ -1,12 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+	var choice int
+
 	fmt.Println("DECIMAL & BINARY & HEXADECIMAL CONVERTER")
 
-	hexToBinary()
-	//binaryToDecimal()
+	fmt.Println("[1] - Binary to Decimal")
+	fmt.Println("[2] - Hexadecimal to Binary")
+	fmt.Print("[0] - Exit\n\n")
+
+	fmt.Print("Enter your choice: ")
+	fmt.Scanln(&choice)
+
+	switch choice {
+	case 1:
+		binaryToDecimal()
+
+	case 2:
+		hexToBinary()
+
+	case 0:
+		os.Exit(0)
+
+	}
+}
+
+func decimalToBinary() {
+	var deciInput int
+
+	fmt.Print("Enter a decimal value: ")
+	fmt.Scanln(&deciInput)
+
 }
 
 func binaryToDecimal() {
@@ -17,6 +47,7 @@ func binaryToDecimal() {
 	array := [9]int{256, 128, 64, 32, 16, 8, 4, 2, 1}
 
 	// Input a binary and store it in the array
+	// Input number "2" to enter the binary inputs
 	fmt.Print("Enter a binary digit: ")
 	for binaryHolder != 2 {
 		ctr++
@@ -24,15 +55,17 @@ func binaryToDecimal() {
 		binaryArr[ctr] = binaryHolder
 	}
 
-	max := 9 // Max size if the array
+	max := 9 // Max size of the array
 	if ctr != 9 {
+		// Moving the binary digits to the LSB
 		for i := ctr; i >= 0; i-- {
-			binaryArr[max] = binaryArr[i]
-			binaryArr[i] = 0
-			max--
+			binaryArr[max] = binaryArr[i] // Changing the indext position of the binary
+			binaryArr[i] = 0              // Changing the past value of the indext to 0
+			max--                         // Decrementing the max for moving the next binary
 		}
 	}
 
+	// Printing the binary values
 	for i := 0; i < 9; i++ {
 		fmt.Print(binaryArr[i])
 	}
@@ -44,20 +77,22 @@ func binaryToDecimal() {
 		}
 	}
 	fmt.Println()
-	fmt.Print(decimal) // Print the decimal value of the binary
+	fmt.Print("Decimal Value: " + strconv.Itoa(decimal)) // Print the converted binary into decimal
 }
 
 func hexToBinary() {
-	//var decimal int
-	var hexArr [4]string
 	var tempHolder string
-	//array := [4]int{8, 4, 2, 1}
+	var hexArr [4]string
+	var binary [16]int
 	var valueArr [4]int
+	arr := [4]int{8, 4, 2, 1}
 
+	// Input a hexadecimal value
 	fmt.Print("Enter a hexadecimal: ")
 	for i := 0; i < 4; i++ {
 		fmt.Scan(&tempHolder)
 
+		// Check if the input hecadecimal is valid
 		if tempHolder == "0" ||
 			tempHolder == "1" ||
 			tempHolder == "2" ||
@@ -74,10 +109,11 @@ func hexToBinary() {
 			tempHolder == "D" ||
 			tempHolder == "E" ||
 			tempHolder == "F" {
-			hexArr[i] = tempHolder
+			hexArr[i] = tempHolder // Store the input hexadecimal to the array
 		}
 	}
 
+	// Loop for converting each hexademical value into its numerical form or transform it into integer values
 	for i := 0; i < 4; i++ {
 		if hexArr[i] == "0" {
 			valueArr[i] = 0
@@ -114,7 +150,39 @@ func hexToBinary() {
 		}
 	}
 
+	// Print the integer values of the hexadecimal
 	for i := 0; i < 4; i++ {
 		fmt.Print(valueArr[i])
+		fmt.Print(" ")
+	}
+
+	ctr, max := 0, 4
+
+	// Nested loop for converting the hexadecimal into its binary form
+	for i := 0; i < 4; i++ { // Outer loop for locating the index of the hexadecimal values
+		value := valueArr[i] // Store the value of a certain index in the "value" variable
+		h := 0
+		for j := ctr; j < max; j++ { // Inner loop for the convertion
+			if value >= arr[h] {
+				value -= arr[h]
+				binary[j] = 1
+			} else if value < arr[h] {
+				binary[j] = 0
+			}
+			h++
+		}
+		ctr += 4 // Starting point of the next convertion
+		max += 4 // Ending point of the convertion
+	}
+
+	fmt.Println()
+
+	// Print the converted hexadecimal into binary
+	fmt.Print("Binary Value: ")
+	for i := 0; i < 16; i++ {
+		if i == 4 || i == 8 || i == 12 {
+			fmt.Print(" ")
+		}
+		fmt.Print(binary[i])
 	}
 }
